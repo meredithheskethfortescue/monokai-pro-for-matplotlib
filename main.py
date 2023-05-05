8cfd68970fc996b90a8a686f8f55fb1ec58e1e53
Meredith Hesketh-Fortescue <30291320+meredithheskethfortescue@users.noreply.github.com>
2023-05-05 12:17:11 +0200

    update and moved dev funcs

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 """Monokai Pro for Matplotlib"""
 import matplotlib.pyplot as plt
 
-
-# get list of available parameters and their current value
-for key, value in zip(plt.rcParams, plt.rcParams.values()):
-	print(f"{key} : {value}")
-
 # palette
-color_base_bg = '#1d2528'
+color_base_bg_light = '#273136'  # rgb(39, 49, 54)
+color_base_bg_dark = '#1d2528'  # rgb(29, 37, 40)
 color_text_white = '#8b9798'
 
 color_gray = '#8b9798'  # rgb(139, 151, 152) todo: find correct comment color
@@ -16,17 +12,18 @@ color_red = '#FF6D7E'  # rgb(255, 109, 126)
 color_orange = '#FFB270'  # rgb(255, 178, 112)
 color_yellow = '#FFED72'  # rgb(255, 237, 114)
 color_green = '#A2E57B'  # rgb(162, 229, 123)
-color_cyan = '#7CD5F1'  # rgb(124, 213, 241)
 color_blue = '#7CD5F1'  # rgb(124, 213, 241)
 color_purple = '#BAA0F8'  # rgb(186, 160, 248)
-color_pink = '#FF6D7E'  # rgb(255, 109, 126)
 
 # set parameter
-plt.rcParams['axes.facecolor'] = color_base_bg
-plt.rcParams['figure.facecolor'] = color_base_bg
+plt.rcParams['axes.facecolor'] = color_base_bg_light
+plt.rcParams['figure.facecolor'] = color_base_bg_dark
 plt.rcParams['axes.edgecolor'] = color_text_white
 plt.rcParams['xtick.color'] = color_text_white
 plt.rcParams['ytick.color'] = color_text_white
+plt.rcParams['axes.titlecolor'] = color_text_white
+plt.rcParams['legend.labelcolor'] = color_text_white
+plt.rcParams['grid.color'] = color_gray
 
 plt.rcParams['axes.prop_cycle'] = plt.cycler(color=[
     color_blue,
@@ -34,8 +31,25 @@ plt.rcParams['axes.prop_cycle'] = plt.cycler(color=[
     color_green,
     color_red,
     color_purple,
-    color_pink,
     color_gray,
     color_yellow,
-    color_cyan,
 ])
+
+if __name__ == '__main__':
+    def main():
+        # get list of available parameters and their current value
+        for key, value in zip(plt.rcParams, plt.rcParams.values()):
+            print(f"{key} : {value}")
+
+        # test plot
+        plt.figure()
+        for it in range(7):
+            plt.plot([0, it], '-o', label=f"$n_{it} = {it}$ lorem ipsum")
+
+        plt.title("Title")
+        plt.grid()
+        plt.legend()
+        plt.show()
+
+
+    main()
