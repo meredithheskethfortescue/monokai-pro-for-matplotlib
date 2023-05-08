258d59f3a6842461d1ba92906af9a963049ce80d
Meredith Hesketh-Fortescue <30291320+meredithheskethfortescue@users.noreply.github.com>
2023-05-08 15:01:09 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+
+setup(
+    name='monokai-pro-matplotlib',
+    version='0.0.1',
+    author="Raphael Stascheit",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[req for req in requirements if req[:2] != "# "],
+    packages=['monokai-pro-matplotlib'],
+    package_data={'monokai-pro': ['machine.mplstyle', 
+                                  'octagon.mplstyle', 
+                                  'ristretto.mplstyle', 
+                                  'spectrum.mplstyle', 
+                                  'classic.mplstyle']},  # plt.style.use('monokai-pro/machine')
+)
