dc438c1949857d90420529598ced287ce9048813
Raphael Stascheit <raphael.stascheit@gmail.com>
2023-05-14 16:18:05 +0200

    new feature: creation from template

--- src/monokai_pro_matplotlib/main.py
+++ src/monokai_pro_matplotlib/main.py
@@ -1,45 +1,69 @@
 """Monokai Pro for Matplotlib"""
+import os
+
 import matplotlib.pyplot as plt
 
 
+class Palette:
+    # todo: Palette.green.as_rgb()
+    # todo: other filter
+    # Filter: Machine
+    bg_light = '273136'  # rgb(39, 49, 54)
+    bg_dark = '1d2528'  # rgb(29, 37, 40)
+    bg_highlight = '313a3e'  # rgb(49 58 62)
+    white = 'f0fffc'  # rgb(240 255 252)
+    comment_gray = '94a2a2'  # rgb(148 162 162)
+    gray = '8b9798'  # rgb(139, 151, 152)
+    red = 'FF6D7E'  # rgb(255, 109, 126)
+    orange = 'FFB270'  # rgb(255, 178, 112)
+    yellow = 'FFED72'  # rgb(255, 237, 114)
+    green = 'A2E57B'  # rgb(162, 229, 123)
+    blue = '7CD5F1'  # rgb(124, 213, 241)
+    purple = 'BAA0F8'  # rgb(186, 160, 248)
+
+
 def overwrite_current_settings():
-    # palette
-    color_base_bg_light = '#273136'  # rgb(39, 49, 54)
-    color_base_bg_dark = '#1d2528'  # rgb(29, 37, 40)
-    color_text_white = '#8b9798'
-
-    color_gray = '#8b9798'  # rgb(139, 151, 152) todo: find correct comment color
-
-    color_red = '#FF6D7E'  # rgb(255, 109, 126)
-    color_orange = '#FFB270'  # rgb(255, 178, 112)
-    color_yellow = '#FFED72'  # rgb(255, 237, 114)
-    color_green = '#A2E57B'  # rgb(162, 229, 123)
-    color_blue = '#7CD5F1'  # rgb(124, 213, 241)
-    color_purple = '#BAA0F8'  # rgb(186, 160, 248)
-
     # set parameter
-    plt.rcParams['axes.facecolor'] = color_base_bg_light
-    plt.rcParams['figure.facecolor'] = color_base_bg_dark
-    plt.rcParams['axes.edgecolor'] = color_text_white
-    plt.rcParams['xtick.color'] = color_text_white
-    plt.rcParams['ytick.color'] = color_text_white
-    plt.rcParams['axes.titlecolor'] = color_text_white
-    plt.rcParams['legend.labelcolor'] = color_text_white
-    plt.rcParams['grid.color'] = color_gray
+    plt.rcParams['axes.facecolor'] = Palette.bg_light
+    plt.rcParams['figure.facecolor'] = Palette.bg_dark
+    plt.rcParams['axes.edgecolor'] = Palette.white
+    plt.rcParams['xtick.color'] = Palette.white
+    plt.rcParams['ytick.color'] = Palette.white
+    plt.rcParams['axes.titlecolor'] = Palette.white
+    plt.rcParams['legend.labelcolor'] = Palette.white
+    plt.rcParams['grid.color'] = Palette.white
 
     plt.rcParams['axes.prop_cycle'] = plt.cycler(color=[
-        color_blue,
-        color_orange,
-        color_green,
-        color_red,
-        color_purple,
-        color_gray,
-        color_yellow,
+        Palette.blue,
+        Palette.orange,
+        Palette.green,
+        Palette.red,
+        Palette.purple,
+        Palette.gray,
+        Palette.yellow,
     ])
+
+
+def create_mplstyle_from_template(palette: Palette, path: str):
+    # read template
+    with open('template_monokai.css', 'r') as file_template:
+        template = file_template.read()
+
+    # fill template
+    # todo: add header to outputfile
+    style_sheet = template.format(**Palette.__dict__)
+
+    # write out
+    dir_out = os.path.dirname(path)
+    if dir_out:
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, 'w+') as file_out:
+        file_out.write(style_sheet)
 
 
 if __name__ == '__main__':
     # overwrite_current_settings()
+    create_mplstyle_from_template(Palette(), 'machine.mplstyle')
 
     def main():
         # get list of available parameters and their current value
@@ -47,7 +71,6 @@ if __name__ == '__main__':
         #     print(f"{key} : {value}")
 
         plt.style.use('./machine.mplstyle')
-        exit()
 
         # test plot
         plt.figure()
